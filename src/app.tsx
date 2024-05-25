8767d0788e14d65433d4b6f3425d9ee3114d7fbd
jodarini <rivasjose9428@gmail.com>
2024-05-25 16:07:33 -0500

    test lazygit

--- src/app.tsx
+++ src/app.tsx
@@ -19,7 +19,7 @@ export default function app() {
     getUsers()
   }, [])
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col bg-black">
       <h1 className="text-purple-700 text-3xl">app</h1>
       {
         users?.map((user) => (
