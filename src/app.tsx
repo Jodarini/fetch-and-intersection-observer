4be60d018ca305683c1eb3c6325a2905e7e0bae6
jodarini <rivasjose9428@gmail.com>
2024-05-25 15:57:10 -0500

    flex

--- src/app.tsx
+++ src/app.tsx
@@ -19,7 +19,7 @@ export default function app() {
     getUsers()
   }, [])
   return (
-    <div className="flex">
+    <div className="flex flex-row">
       <h1 className="text-purple-700 text-3xl">app</h1>
       {
         users?.map((user) => (
