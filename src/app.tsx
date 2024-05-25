92234f28d0bff91893b21be7d6aff9f6572cdae1
jodarini <rivasjose9428@gmail.com>
2024-05-25 15:55:15 -0500

    flex

--- src/app.tsx
+++ src/app.tsx
@@ -19,7 +19,7 @@ export default function app() {
     getUsers()
   }, [])
   return (
-    <>
+    <div className="flex">
       <h1 className="text-purple-700 text-3xl">app</h1>
       {
         users?.map((user) => (
@@ -30,7 +30,7 @@ export default function app() {
           </div>
         ))
       }
-    </>
+    </div>
   )
 }
 
