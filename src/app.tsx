7f845a9e4898ad54a1c881bde6144997e989249c
jodarini <rivasjose9428@gmail.com>
2024-05-24 20:56:23 -0500

    First fetch

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,36 @@
+import { useEffect, useState } from "react"
+
+interface User {
+  firstName: string
+  lastName: string
+  id: number
+  image: string
+}
+
 export default function app() {
+  const [users, setUsers] = useState<User[]>()
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const res = await fetch('http://dummyjson.com/users?&limit=10')
+      const data = await res.json()
+      setUsers(data.users)
+    }
+    getUsers()
+  }, [])
   return (
-    <div className="text-purple-700 text-3xl">app</div>
+    <>
+      <h1 className="text-purple-700 text-3xl">app</h1>
+      {
+        users?.map((user) => (
+          <div key={user.id}>
+            <p>{user.firstName}</p>
+            <p>{user.lastName}</p>
+            <img src={user.image}></img>
+          </div>
+        ))
+      }
+    </>
   )
 }
 
