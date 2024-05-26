9a55b19e0d616bfd2e3a2e791bc6c4a5968bd313
jodarini <rivasjose9428@gmail.com>
2024-05-26 18:16:09 -0500

    Implement search input

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react"
+import { ChangeEventHandler, FormEvent, FormEventHandler, useEffect, useRef, useState } from "react"
 
 interface Users {
   firstName: string
@@ -12,6 +12,7 @@ export default function app() {
   const observedRef = useRef()
   const [skip, setSkip] = useState(0)
   const [hasMore, setHasMore] = useState(true)
+  const [searchValue, setSearchValue] = useState()
 
   const intersectionCallback = (entries: IntersectionObserverEntry[]) => {
     entries.forEach(entry => {
@@ -42,9 +43,28 @@ export default function app() {
     }
   }
 
+  const searchUsers = async () => {
+    const res = await fetch(`http://dummyjson.com/users/search?q=${searchValue}`)
+    const data = await res.json()
+    if (data.users.length === 0) {
+      setHasMore(false)
+    } else {
+      setUsers(data.users)
+      setSkip(prev => prev + 1)
+    }
+  }
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    searchUsers()
+  }
   return (
     <div className="flex flex-col gap-4">
       <h1 className="text-3xl">app</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder=" search..." onChange={e => setSearchValue(e.target.value)} name={searchValue} />
+      </form>
+
       {
         users?.map((user) => (
           <div key={user.id}>
